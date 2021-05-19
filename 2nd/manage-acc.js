e762c39091a2118c36f4017a85c5f1e4272a5563
Tsega <tsega207@gmail.com>
2021-05-19 15:13:37 +0300

    updates

--- 2nd/manage-acc.js
+++ 2nd/manage-acc.js
@@ -1,3 +1,4 @@
+// TODO: Make sure the center content doesn't intersect the security and personal pannels css problem
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
